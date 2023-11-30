13672478498d659ffd9c44d6de907993cf2bbfd0
Cássia <cascrisbern@gmail.com>
2023-11-30 12:06:24 -0300

    added: used useEffect to monitor the size of the screen, so on resize, if mobile menu is open, it will be closed. also added onClick function to close the mobile menu when clicked on the menu item

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,24 @@
 import styles from "./Navbar.module.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { RxHamburgerMenu } from "react-icons/rx"
 
 export function Navbar() {
 	const [isNavExpanded, setIsNavExpanded] = useState(false)
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth > 1024 && isNavExpanded) {
+				setIsNavExpanded(!isNavExpanded)
+			}
+		}
+
+		window.addEventListener("resize", handleResize)
+
+		return () => {
+			window.removeEventListener("resize", handleResize)
+		}
+	}, [isNavExpanded])
+
 	return (
 		<nav className={styles.navigation}>
 			<a className={styles.brandName} href="/">
@@ -23,13 +38,19 @@ export function Navbar() {
 			>
 				<ul>
 					<li>
-						<a href="#work">WORK</a>
+						<a href="#work" onClick={() => setIsNavExpanded(!isNavExpanded)}>
+							WORK
+						</a>
 					</li>
 					<li>
-						<a href="#about">ABOUT</a>
+						<a href="#about" onClick={() => setIsNavExpanded(!isNavExpanded)}>
+							ABOUT
+						</a>
 					</li>
 					<li>
-						<a href="#contact">CONTACT</a>
+						<a href="#contact" onClick={() => setIsNavExpanded(!isNavExpanded)}>
+							CONTACT
+						</a>
 					</li>
 				</ul>
 			</div>
