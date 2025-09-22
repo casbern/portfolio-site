5c3b78bc36d5591993e9704b945b5df66b846515
Cássia <cascrisbern@gmail.com>
2025-09-22 16:45:35 +0300

    added new project refactor: moved projects data to separate file and render dynamically

---
+++ src/data/projects.js
@@ -0,0 +1,163 @@
+export const projects = [
+  {
+    color: 'var(--pink-200)',
+    title: 'Waste Collection Service Redesign',
+    content:
+      'A responsive redesign page that enhances UX while maintaining all required functionalities. Created with modern, clean architecture and smooth animations as part of a job application challenge.',
+    technologies:
+      'ReactJS • JavaScript • Tailwind • Axios • Lucide React • Motion',
+    link: 'https://rwproject.netlify.app/',
+    github: 'https://github.com/casbern/rem-waste-project',
+  },
+  {
+    color: 'var(--blue-200)',
+    title: 'Crowdfund Project',
+    content:
+      'The project’s main objective is to allow users to make pledges, manage product bookmarks, and interact with hover states on key elements.',
+    technologies: 'ReactJS • JavaScript • Tailwind • TypeScript',
+    link: 'https://crowdfounding-proj.netlify.app/',
+    github: 'https://github.com/casbern/crowdfund-page',
+  },
+  {
+    color: 'var(--pink-200)',
+    title: 'Ignite Shop',
+    content:
+      'A simple, responsive t-shirt shopping app where you can add items to the cart, calculate the total and quantity, and finalize the order through the Stripe API.',
+    technologies: 'Next.js • TypeScript • JavaScript • Tailwind • Stripe',
+    link: 'https://shopignite.netlify.app/',
+    github: 'https://github.com/casbern/ignite-shop',
+  },
+  {
+    color: 'var(--lilac)',
+    title: 'Timer with History',
+    content:
+      'A different countdown for controlling your tasks. Here you can give a name to your task and check it on the history tab for your own control.',
+    technologies: 'React • Styled-Component • JavaScript • TypeScript • HTML',
+    link: 'https://ignit-timer.netlify.app/',
+    github: 'https://github.com/casbern/timer',
+  },
+  {
+    color: 'var(--pink-200)',
+    title: 'Coffee Delivery Store',
+    content:
+      'A delivery coffee store in which the client can place an order, choose the delivery and the payment method.',
+    technologies:
+      'React • Styled-Component • JavaScript • TypeScript • HTML • CSS • Radix',
+    link: 'https://coffeedelivery.netlify.app/',
+    github: 'https://github.com/casbern/coffee-delivery',
+  },
+  {
+    color: 'var(--blue-200)',
+    title: 'Github Blog Issues',
+    content:
+      'A very nice application that shows all the issues related to a repo. We can use the search to look faster to an specific issue and click on it for more details.',
+    technologies:
+      'React • Styled-Component • JavaScript • TypeScript • HTML • CSS',
+    link: 'https://gitbloghub.netlify.app/',
+    github: 'https://github.com/casbern/github-blog',
+  },
+  {
+    color: 'var(--pink-200)',
+    title: 'Food Explorer',
+    content: `A restaurant web app that accepts food delivery orders, 
+							lists the dishes and has an admin area to manage the dishes.
+							To test the app, it's necessary to create an account.`,
+    technologies:
+      'React • Express • Styled-components • SQLite • CSS • HTML • JavaScript',
+    link: 'https://rocketseat-foodexplorer.netlify.app/',
+    github: 'https://github.com/casbern/foodexplorer-frontend',
+  },
+  {
+    color: 'var(--lilac)',
+    title: 'Focus Timer',
+    content:
+      'A fully responsive web app allowing time manipulation, music playback, and seamless light/dark mode toggling for optimal user experience across devices.',
+    technologies: 'HTML • CSS • JavaScript',
+    link: 'https://timertofocus.netlify.app/',
+    github: 'https://github.com/casbern/focus-timer-dark-mode',
+  },
+  {
+    color: 'var(--pink-200)',
+    title: 'Todo App',
+    content:
+      'A to-do app that marks all your tasks for the day and includes a counter that tracks how many tasks have been completed and how many are still open to finish.',
+    technologies: 'HTML • CSS • JavaScript • React • Typescript',
+    link: 'https://todo-tasks-project.netlify.app/',
+    github: 'https://github.com/casbern/todo',
+  },
+  {
+    color: 'var(--blue-200)',
+    title: 'Rocket Notes',
+    content: `A web app for storing personal notes. 
+							It's possible to edit and delete notes as needed, as well as edit your profile and photo.
+							To test the app, it's necessary to create an account.`,
+    technologies:
+      'React • Express • Styled-components • SQLite • CSS • HTML • JavaScript',
+    link: 'https://keepthenotes.netlify.app/',
+    github: 'https://github.com/casbern/explorer-rocketnotes-frontend',
+  },
+  {
+    color: 'var(--lilac)',
+    title: 'Lucky Cookie',
+    content:
+      'A super fun responsive web app that emulates a virtual Chinese fortune cookie experience, delivering wise life tips. Try it out!',
+    technologies: '',
+    link: 'https://luckycookie.netlify.app/',
+    github: 'https://github.com/casbern/biscoito-da-sorte',
+  },
+  {
+    color: 'var(--pink-200)',
+    title: 'Four Cards Layout',
+    content:
+      'A fully responsive card layout focusing on semantic HTML5 markup, CSS Grid, CSS Grid Area, and a mobile-first approach.',
+    technologies: 'HTML • CSS • JavaScript',
+    link: 'https://cards-layout.netlify.app/',
+    github: 'https://github.com/casbern/four-card-feature-seaction-master',
+  },
+  {
+    color: 'var(--blue-200)',
+    title: 'Pricing Component with Toggle',
+    content:
+      'A fully responsive pricing component that toggles between monthly and annual prices, emphasizing semantic HTML5, CSS Flexbox, JavaScript, and a mobile-first approach.',
+    technologies: 'HTML • CSS • JavaScript',
+    link: 'https://pricing-toggle-layout.netlify.app/',
+    github: 'https://github.com/casbern/pricing-component-with-toggle-master',
+  },
+  {
+    color: 'var(--pink-200)',
+    title: 'Sign Up Form with JavaScript Validation',
+    content:
+      'This project utilized HTML, CSS, and JavaScript, following a mobile-first approach.',
+    technologies: 'HTML • CSS • JavaScript',
+    link: 'https://sign-up-form-js-validation.netlify.app/',
+    github: 'https://github.com/casbern/sign-up-form',
+  },
+  {
+    color: 'var(--lilac)',
+    title: '3 Column Card Component',
+    content:
+      'A three card component layout that used semantic HTML, CSS Flexbox and a mobile-first workflow.',
+    technologies: 'HTML • CSS',
+    link: 'https://three-column-card-component-layout.netlify.app/',
+    github: 'https://github.com/casbern/3-column-preview-card-component-main',
+  },
+  {
+    color: 'var(--blue-200)',
+    title: 'Sunnyside Agency Landing Page',
+    content:
+      'A fully responsive landing page for a marketing agency, emphasizing semantic HTML5, CSS Flexbox, CSS Grid, JavaScript, and a mobile-first approach.',
+    technologies: 'HTML • CSS • JavaScript',
+    link: 'https://sunnyside-agency-landpage.netlify.app/',
+    github: 'https://github.com/casbern/sunnyside-agency',
+  },
+  // {
+  //   color: '',
+  //   title: 'Reta Final',
+  //   content:
+  //     'A web application built as a final university project for a company that manages tournaments between soccer schools.',
+  //   technologies:
+  //     'HTML • CSS • JavaScript • Node.js • Nunjucks • Express • PostgreSQL',
+  //   link: 'https://reta-final.onrender.com/campeonatos',
+  //   github: 'https://github.com/casbern/reta-final',
+  // },
+]
